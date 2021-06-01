bfe0cdde33cf2d7e82855e782451f61f0a2c9f46
duytranndev <duytrann.dev@gmail.com>
2021-06-01 22:50:07 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+import './App.scss'
+import AppLayout from './ui/templates/layout'
+
+function App() {
+  return (
+    <Fragment>
+      <AppLayout />
+    </Fragment>
+  )
+}
+
+export default App
---
+++ src/ui/templates/layout/index.tsx
@@ -0,0 +1,10 @@
+import AppRouting from '../../../share/routing'
+
+const AppLayout = (): JSX.Element => {
+  return (
+    //   header
+    <AppRouting />
+    //footer
+  )
+}
+export default AppLayout
